e90f2d36f57120203e6e2342026b4837bbfa5b67
oscar <oscar.carrillo758@gmail.com>
2025-04-01 23:12:15 -0700

    seventh commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 
 export const searchRecipes = async (searchTerm, page) => {
